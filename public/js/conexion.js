a90ed12f318baa0577429d85d0fa9fa0df514cc3
Jonathan Hernandez <jonathanhernandez@jonathans-imac.gateway.2wire.net>
2017-06-26 17:56:55 -0500

    Torremi V1.0

---
+++ public/js/conexion.js
@@ -0,0 +1,9 @@
+  var config = {
+    apiKey: "AIzaSyBk0BcfYHkXAR1bnLziGPdzKRUu5K2MzmE",
+    authDomain: "torremi-ff0da.firebaseapp.com",
+    databaseURL: "https://torremi-ff0da.firebaseio.com",
+    projectId: "torremi-ff0da",
+    storageBucket: "torremi-ff0da.appspot.com",
+    messagingSenderId: "332569012408"
+  };
+  firebase.initializeApp(config);
